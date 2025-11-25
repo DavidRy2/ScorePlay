cf9095f0bad1065121595607bb4bf714a0b99fa9
David Ryan <seu-email@exemplo.com>
2025-11-25 17:04:59 -0300

    Update no design da aplicação

--- Score/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { Inicio } from './inicio/inicio';
-import { Login } from './login/login';
-
-export const routes: Routes = [
-  { path: '', component: Login },   // LOGIN primeiro!
-  { path: 'home', component: Inicio } // Home só depois de logar
-];
